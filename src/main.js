cd0b088eeb2c204c27a1fa621258a198c8a6d3aa
techninja <james@tn42.com>
2018-05-31 14:08:54 -0700

    Add Draggable Foods

--- src/main.js
+++ src/main.js
@@ -12,16 +12,19 @@ const desktopSkin = new Skin({ fill: "blue" });
 const babsTexture = new Texture("babs.png");
 const babsSkin = new Skin({ texture: babsTexture, x:0, y:0, width: 215, height: 178});
 
+const foodTexture = new Texture("foods.png");
+const foodSkin = new Skin({ texture: foodTexture, x:0, y:0, width: 60, height: 60, variants:60});
+
 const buttonTexture = new Texture("button.png");
 const buttonSkin = new Skin({ texture:buttonTexture, x:0, y:0, width:60, height:40, states:40, tiles:{ left:20, right:20 } });
 const buttonStyle = new Style({ font:"600 28px Open Sans", color:["red", "white"], });
 
 class DragBehavior extends Behavior {
   onFinished(content) {
-    content.state = 0;
+    //content.state = 0;
   }
   onTimeChanged(content) {
-    content.state = 2 - Math.quadEaseOut(content.fraction);
+    //content.state = 2 - Math.quadEaseOut(content.fraction);
   }
   onTouchBegan(content, id, x, y, ticks) {
     let anchor = this.anchor = content.position;
@@ -34,7 +37,7 @@ class DragBehavior extends Behavior {
     content.position = { x:anchor.x + x, y:anchor.y + y };
   }
   onTouchEnded(content, id, x, y, ticks) {
-    content.duration = 250;
+    //content.duration = 250;
     content.time = 0;
     content.start();
   }
@@ -45,10 +48,10 @@ const sideBar = $ =>
     left: 0, top:0, width: 60, height: 240,
     skin: new Skin({fill: "red"}),
     contents: [
-      Label($, {left:10, top:10, width:40, height:40, skin:buttonSkin, string:"A", active:true, Behavior:DragBehavior}),
-      Label($, {left:10, top:70, width:40, height:40, skin:buttonSkin, string:"B", active:true, Behavior:DragBehavior}),
-      Label($, {left:10, top:130, width:40, height:40, skin:buttonSkin, string:"C", active:true, Behavior:DragBehavior}),
-      Label($, {left:10, top:190, width:40, height:40, skin:buttonSkin, string:"D", active:true, Behavior:DragBehavior}),
+      Content(4, {left:0, top:0, skin:foodSkin, active:true, variant:0, Behavior:DragBehavior}),
+      Content(3, {left:0, top:60, skin:foodSkin, active:true, variant:1, Behavior:DragBehavior}),
+      Content(2, {left:0, top:120, skin:foodSkin, active:true, variant:2, Behavior:DragBehavior}),
+      Content(1, {left:0, top:180, skin:foodSkin, active:true, variant:3, Behavior:DragBehavior}),
     ]
   });
 
